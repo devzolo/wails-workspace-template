9031d1aeeb8ee507aa543672a67d544fc82c327f
devzolo <13318936+devzolo@users.noreply.github.com>
2023-10-20 21:58:44 -0300

    update svelte.config.js, tsconfig.json and vite.config.ts

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
 	server: {
@@ -12,8 +13,8 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	resolve: {
     alias: {
-      '~': './src', 
-    },
+			'~': path.resolve('./src'),
+		},
   },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
